110ee5d15853754cff58d7e0d856093a8c7ca9a5
YuYing Chen <y.chen.66@student.rug.nl>
2018-05-01 12:16:16 +0200

    update loading files

---
+++ attempt/pathTest.py
@@ -0,0 +1,6 @@
+import csv
+
+with open('../benchmark_datasets/Abalone/abalone.data', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
