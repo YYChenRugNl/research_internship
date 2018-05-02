2068952cac2c2ee9d88e504b92026091ca7500cf
YuYing Chen <y.chen.66@student.rug.nl>
2018-05-02 17:21:39 +0200

    add artificial data generator

--- attempt/test.py
+++ attempt/test.py
@@ -7,28 +7,34 @@ from glvq import GmlvqModel, plot2d, tools
 print(__doc__)
 
 datapath = '../benchmark_datasets/Abalone/abalone.data'
-datapath = '../benchmark_datasets/Bank/Bank32nh/bank32nh.data'
+# datapath = '../benchmark_datasets/Bank/Bank32nh/bank32nh.data'
 domainpath = '../benchmark_datasets/Abalone/abalone.domain'
 tools = tools.CustomTool()
-# toy_data, toy_label = tools.read_from_file(datapath, 0)
-toy_data, toy_label = tools.read_from_file2(datapath)
+toy_data, toy_label = tools.read_from_file(datapath, 0)
+# toy_data, toy_label = tools.read_from_file2(datapath)
 # toy_label[toy_label > 0.666] = 2
 # toy_label[(toy_label > 0.333) & (toy_label <= 0.666)] = 1
 # toy_label[toy_label <= 0.333] = 0
 
-for i in range(4):
-    percent = i * 25
-    fraction = np.percentile(toy_label, percent)
-    print(fraction)
+# for i in range(4):
+#     percent = i * 25
+#     fraction = np.percentile(toy_label, percent)
+#     print(fraction)
 
-toy_label[toy_label > 0.5] = 1
+# toy_label[toy_label > 0.5] = 1
 # toy_label[(toy_label > 0.1509558) & (toy_label <= 0.2602056)] = 6
 # toy_label[(toy_label > 0.0862336) & (toy_label <= 0.1509558)] = 5
 # toy_label[(toy_label > 0.0484986) & (toy_label <= 0.0862336)] = 4
 # toy_label[(toy_label > 0.025383) & (toy_label <= 0.0484986)] = 3
 # toy_label[(toy_label > 0.0254) & (toy_label <= 0.1126)] = 2
 # toy_label[(toy_label > 0.0019) & (toy_label <= 0.0254)] = 1
-toy_label[toy_label <= 0.5] = 0
+# toy_label[toy_label <= 0.5] = 0
+
+list_center = [[0, 0], [3, 3], [4, -1]]
+basic_matrix = [[3, 0], [0, 3]]
+list_matrix = [[[3, 1], [1, 3]], basic_matrix, basic_matrix]
+number_sample = 100
+toy_data, toy_label = tools.artificial_data(number_sample, list_center, list_matrix)
 
 print(toy_data)
 print(toy_label)
--- glvq/tools.py
+++ glvq/tools.py
@@ -23,3 +23,18 @@ class CustomTool():
         # print(attr)
         # print(labels)
         return attr, labels[:, 0]
+
+    def artificial_data(self, sample_size, list_center, list_matrix):
+        nb_ppc = sample_size
+        toy_label = np.zeros(nb_ppc)
+        toy_data = np.random.multivariate_normal(list_center[0], np.array(list_matrix[0]), size=nb_ppc)
+        k = len(list_center)
+        if k > 0:
+            for i in range(k - 1):
+                index = i + 1
+                toy_label = np.append(toy_label, np.ones(nb_ppc) * index, axis=0)
+                toy_data = np.append(toy_data,
+                                     np.random.multivariate_normal(list_center[index], np.array(list_matrix[index]),
+                                                                   size=nb_ppc), axis=0)
+
+        return toy_data, toy_label
